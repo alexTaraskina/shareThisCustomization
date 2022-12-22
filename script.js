5a47c0fc35a66d067b0f29ea15ec67616768366c
Alexandra Taraskina <serzhanina_aleks@mail.ru>
2022-12-22 15:41:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let facebookIcon = document.querySelector('img.facebook');
+    if (facebookIcon) {
+        facebookIcon.src = "./facebook.svg";
+    }
+});
